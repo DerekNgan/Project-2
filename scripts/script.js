770c54f4562657237380a352b65bb5b71e14e10a
solivanlau <solivan_lau@outlook.com>
2023-03-03 16:34:30 -0500

    firebase integration - solved scope issue with currentAnswer - need to condense code

--- scripts/script.js
+++ scripts/script.js
@@ -1,172 +1,158 @@
 import firebaseInfo from "./firebaseConfig.js"
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+import { get, getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
 
 const database = getDatabase(firebaseInfo);
 const dbRef = ref(database);
 
-// DATA FOR QUESTIONS AND ANSWERS
-const fourAnswerQuestions = [
+// SPECIFIC OBJECT REFS
+// const fourAnswerRef = ref(database, `/fourAnswerQuestions`);
 
-    {
-        question: 'is this test working?',
-        answers: [
-            'yes', 'no', 'maybe', 'so'
-        ],
-        correctAnswer: 1
-    },
+// const trueFalseRef = ref(database, `/trueFalseQuestions`);
 
-    {
-        question: 'TWO: what is the fastest animal',
-        answers: [
-            'turtle', 'cheetah', 'walrus', 'pig'
-        ],
-        correctAnswer: 2
-    },
+// const questionTypeRef = ref(database, `/questionType`);
 
-    {
-        question: 'THREE what is 7 - 8?',
-        answers: [
-            '1', '4', '-2', '-1'
-        ],
-        correctAnswer: 4
-    }
-]
-const trueFalseQuestions = [
+const currentAnswerRef = ref(database, `/currentAnswer`)
+const scoreRef = ref(database, `/score`);
 
-    {
-        question: 'is this test working?',
-        answers: [
-            'yes', 'no'
-        ],
-        correctAnswer: 1
-    },
+// HTML VARIABLES
+const questionDisplay = document.getElementById('question')
 
-    {
-        question: 'TWO: what is the opposite of north',
-        answers: [
-            'north', 'south'
-        ],
-        correctAnswer: 2
-    },
+const answerBox = document.getElementById('answersContainer');
 
-    {
-        question: 'THREE what side of the road do Canadians drive on?',
-        answers: [
-            'Left', 'Right'
-        ],
-        correctAnswer: 2
-    }
-]
 
-const questionType = [
-    fourAnswerQuestions, trueFalseQuestions
-]
+// MISC VARIABLES
+let currentCorrectAnswer = '';
+let localScore = 0;
 
-// DATA FOR USER SCORE
-const score = {
-    userScore: 0
-};
+// FUNCTIONS
+
 
 function randomArrVal(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length)
     return arr[randomIndex]
 }
 
-// call this function when want new set of questions to load
-function questionLoad() {
-    // GENERATE RANDOM QUESTION
-
-    // Generate a RANDOM QUESTION ARRAY from questiontype array -> returns an entire array
-    const currentQuestionType = randomArrVal(questionType);
-    // console.log(currentQuestionType);
-    // generate RANDOM QUESTION OBJECT from selected question type -> returns question object
-    const currentQuestionObj = randomArrVal(currentQuestionType);
-    // console.log(currentQuestionObj)
-
-    // UPDATE QUESTION TEXT
-    // variable for question element (h2)
-    const questionDisplay = document.getElementById('question')
-    // console.log(questionDisplay);
-
-    // using SELECTED QUESTION OBJ and updating question text content
-    questionDisplay.textContent = currentQuestionObj.question
-
-    // DYNAMIC UPDATING EACH ANSWER
-
-    // 1) variable for answer box
-    const currentAnswers = currentQuestionObj.answers
-    // creating an element for EACH item in our answers array
-
-    const answerBox = document.getElementById('answersContainer');
-    // clearing all items in answer box === clearing all html elements in the box
-    answerBox.innerHTML = '';
-
-    currentAnswers.forEach(function (answer) {
-
-        // 2) create button element 
-        const createButton = document.createElement('button')
-        // 3) adjust button w VALUE base on its index in the answers array + 1
-        // button USER EXPERIENCE
-        // adding string value from currentanswers array to our button AS TEXT
-        createButton.textContent = answer;
-
-        // button properties
-        // id
-        createButton.id = `btn${currentAnswers.indexOf(answer) + 1}`;
-        // value
-        createButton.value = currentAnswers.indexOf(answer) + 1;
-
-        // console.log(createButton)
-
-        // 4) append to answer container 
-        answerBox.appendChild(createButton);
-    })
-
-    // NEXT FUNCTIONALITY: Move our data to Firebase
-    // // consider having our data in a JSON file - import it once into firebase!
-    // // refactor ALL paths with finding our data (from question load function). make sure we are interacting with the firebase data
-
-
-
-
-    // NEXT FUNCTIONALITY: user click = answer evaluation
-    // // GOAL: when the user clicks on an answer, check IF the button value === correct answer value
-
-    function answerEval(event) {
-        // establish button is being clicked
-        if (event.target.tagName === 'BUTTON') {
-
-            //converting value to number to compare
-            const currentButton = parseInt(event.target.value);
-
-            // console.log(currentButton);
-
-            const currentCorrectAnswer = currentQuestionObj.correctAnswer
-            if (currentButton === currentCorrectAnswer) {
-                // // // // IF the button value === correct answer value, grant user score +1 point AND add a class of correct to the label
-
-                console.log('yeeeaaaa boiii');
-                score.userScore++;
-                // console.log(score.userScore)
-            } else {
-                // // // // ELSE user score -1 dd a class of correct to the label
-                console.log('nooooo boiii');
-                score.userScore--;
-                // console.log(score.userScore)
-            }
-
-            console.log(score.userScore)
-        }
-
-    }
-
-    answerBox.addEventListener('click', answerEval)
-    // // // listen for a click in PARENT OBJECT for buttons (since they are generated after pageload, we cannot have an eventlistener directly linked)
-
-
+const changeSetting = (ref, settingChange) => {
+    return update(ref, settingChange)
 }
 
-questionLoad();
+function getQuestion() {
+    get(dbRef).then(function (snapshot) {
+        if (snapshot.exists()) {
+            //loggin the entire object
+            const mainObj = snapshot.val()
+            // console.log(mainObj)
+            const questionTypeRef = mainObj.questionType;
+
+            // GENERATING RANDOM QUESTION TYPE
+            const randomQType = randomArrVal(questionTypeRef)
+
+            // dot notation TAKES THINGS LITERALLY
+            // bracket notation takes things FIGURATIVELY
+
+            // console.log(mainObj[randomQType])
+
+            // GENERATING RANDOM QUESTION FROM SELECTED QUESTION TYPE
+            const randomQuestion = randomArrVal(mainObj[randomQType])
+
+            // console.log(randomQuestion)
+
+            // HTML: POPULATE QUESTION 
+            questionDisplay.textContent = randomQuestion.question
+
+            // HTML: POPULATE EACH ANSWER DYNAMICALLY
+
+            // all possible answers FOR random questions
+            const currentAnswersArr = randomQuestion.answers
+            console.log(currentAnswersArr)
+
+            // target each answer -> forEach
+            // make a button for each answer
+            // put id and value for each button
+            // value = index + 1
+            answerBox.innerHTML = '';
+            currentAnswersArr.forEach(function (answer) {
+                const newBtn = document.createElement('button')
+                newBtn.id = `btn${currentAnswersArr.indexOf(answer) + 1}`;
+                newBtn.value = currentAnswersArr.indexOf(answer) + 1;
+                // display answer inside button
+                newBtn.textContent = answer;
+                // console.log(newBtn)
+                // append each button to answerBox
+                answerBox.appendChild(newBtn)
+            })
+
+            const correctObj = { currentAnswer: randomQuestion.correctAnswer };
+
+            update(dbRef, correctObj);
+
+
+        } else {
+            console.log('OAUR NAUR - NAUR QUESTIN AUR AURNSWER');
+        }
+    })
+}
+
+
+getQuestion();
+
+function answerEval(event) {
+    // ONLY ON BTN CLICK
+    if (event.target.tagName === 'BUTTON') {
+
+        const clickedAnswer = parseInt(event.target.value)
+
+        // GET DB CURRENT ANSWER
+        get(dbRef).then(function (snapshot) {
+
+            if (snapshot.exists()) {
+                const currentAnswerPath = snapshot.val().currentAnswer
+
+                // comparing clicked btn value to DB current answer
+                if (clickedAnswer === currentAnswerPath) {
+                    console.log('yippy')
+                    localScore++;
+                    console.log(localScore)
+                    //update DB score
+                    // run get question
+                    getQuestion();
+                } else {
+                    console.log('fuck')
+                    localScore--;
+                    console.log(localScore)
+                    //update DB score
+                    // run get question
+                    getQuestion();
+                }
+
+            } else {
+                console.log('cannot reach database - current answer')
+            }
+        })
+
+    }
+}
+
+
+
+answerBox.addEventListener('click', answerEval)
+
+// get() -> get current score data
+//  add 1 to current score data AS OBJ
+// update DB w new obj
+
+
+// ----------------------------------------------
+
+
+// onValue func for user score! to immediately get most up to date score
+
+
+
+// call this function when want new set of questions to load
+
+
+// questionLoad();
 
 const startBtn = document.querySelector('#startBtn');
 const playBtn = document.querySelector('#playBtn');
@@ -175,23 +161,23 @@ const startState = document.querySelector('#start');
 const playState = document.querySelector('#play');
 const resetState = document.querySelector('#reset');
 
-function changeState (hiddenState, activeState) {
+function changeState(hiddenState, activeState) {
     // when start button is pressed, start state is hidden and play state appears
     hiddenState.classList.add('hidden');
     activeState.classList.remove('hidden');
 }
 
-startBtn.addEventListener('click', function(){
-// hiding start state and activating playstate
+startBtn.addEventListener('click', function () {
+    // hiding start state and activating playstate
     changeState(startState, playState);
     // timer start
-    
-    })
+
+})
 
 //after timer end run changeState() so now the hidden state will be play state and the active state is the reset state
-resetBtn.addEventListener('click', function(){
+resetBtn.addEventListener('click', function () {
     // hiding start state and activating playstate
-        changeState(resetState, startState);
-        // timer start
-    })
+    changeState(resetState, startState);
+    // timer start
+})
 // once at reset state when reset button is pressed active state will be the start state
--- scripts/test-prototype.js
+++ scripts/test-prototype.js
@@ -93,56 +93,94 @@ function randomArrVal(arr) {
 const questionDisplay = document.querySelector('#questionDisplay')
 
 function questionLoad() {
-    // randomly choose type of question 
+    // GENERATE RANDOM QUESTION
+
+    // Generate a RANDOM QUESTION ARRAY from questiontype array -> returns an entire array
     const currentQuestionType = randomArrVal(questionType);
-    console.log(currentQuestionType);
-
-    // randomly choosing a question form question types
+    // console.log(currentQuestionType);
+    // generate RANDOM QUESTION OBJECT from selected question type -> returns question object
     const currentQuestionObj = randomArrVal(currentQuestionType);
-    // appending question.obj items to page
+    // console.log(currentQuestionObj)
 
-    // question update
-    questionDisplay.textContent = currentQuestionObj.question;
+    // UPDATE QUESTION TEXT
+    // variable for question element (h2)
+    const questionDisplay = document.getElementById('question')
+    // console.log(questionDisplay);
 
-    // dynamic answer update
+    // using SELECTED QUESTION OBJ and updating question text content
+    questionDisplay.textContent = currentQuestionObj.question
+
+    // DYNAMIC UPDATING EACH ANSWER
+
+    // 1) variable for answer box
     const currentAnswers = currentQuestionObj.answers
-    const answerBox = document.querySelector('#answerBox')
-    // clearing answers
+    // creating an element for EACH item in our answers array
+
+    const answerBox = document.getElementById('answersContainer');
+    // clearing all items in answer box === clearing all html elements in the box
     answerBox.innerHTML = '';
 
-
     currentAnswers.forEach(function (answer) {
-        // console.log(currentAnswers.indexOf(answer)+1);
-        const createDiv = document.createElement('div')
-        createDiv.innerHTML = `
-        <label for="btn${currentAnswers.indexOf(answer) + 1}">
-            ${answer}
-            <input type="radio" name="btn${currentAnswers.indexOf(answer) + 1}" id="btn${currentAnswers.indexOf(answer) + 1}" value="${currentAnswers.indexOf(answer) + 1}" class="answerInput">
-        </label>`
 
-        answerBox.appendChild(createDiv)
+        // 2) create button element 
+        const createButton = document.createElement('button')
+        // 3) adjust button w VALUE base on its index in the answers array + 1
+        // button USER EXPERIENCE
+        // adding string value from currentanswers array to our button AS TEXT
+        createButton.textContent = answer;
+
+        // button properties
+        // id
+        createButton.id = `btn${currentAnswers.indexOf(answer) + 1}`;
+        // value
+        createButton.value = currentAnswers.indexOf(answer) + 1;
+
+        // console.log(createButton)
+
+        // 4) append to answer container 
+        answerBox.appendChild(createButton);
     })
 
+    // NEXT FUNCTIONALITY: Move our data to Firebase
+    // // consider having our data in a JSON file - import it once into firebase!
+    // // refactor ALL paths with finding our data (from question load function). make sure we are interacting with the firebase data
 
-    const currentCorrectAnswer = currentQuestionObj.correctAnswer
+
+
+
+    // NEXT FUNCTIONALITY: user click = answer evaluation
+    // // GOAL: when the user clicks on an answer, check IF the button value === correct answer value
 
     function answerEval(event) {
-        // Input event listener
-        if (event.target.tagName === 'INPUT') {
-            const userAnswer = parseInt(event.target.value)
-            console.log(event.target);
-            console.log(userAnswer)
-            if (userAnswer === currentCorrectAnswer) {
-                console.log('you are correct')
-                setTimeout(questionLoad, 500);
+        // establish button is being clicked
+        if (event.target.tagName === 'BUTTON') {
 
+            //converting value to number to compare
+            const currentButton = parseInt(event.target.value);
+
+            // console.log(currentButton);
+
+            const currentCorrectAnswer = currentQuestionObj.correctAnswer
+            if (currentButton === currentCorrectAnswer) {
+                // // // // IF the button value === correct answer value, grant user score +1 point AND add a class of correct to the label
+
+                console.log('yeeeaaaa boiii');
+                score.userScore++;
+                // console.log(score.userScore)
             } else {
-                console.log('you are WRONG')
-                // questionLoad()
-                setTimeout(questionLoad, 500);
+                // // // // ELSE user score -1 dd a class of correct to the label
+                console.log('nooooo boiii');
+                score.userScore--;
+                // console.log(score.userScore)
             }
+
+            console.log(score.userScore)
         }
+
     }
 
     answerBox.addEventListener('click', answerEval)
+    // // // listen for a click in PARENT OBJECT for buttons (since they are generated after pageload, we cannot have an eventlistener directly linked)
+
+
 }
